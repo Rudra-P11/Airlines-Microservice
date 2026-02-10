03b6b808a1dcebc9f56318ee7873e07b57f8b09b
Rudra P <pandyarudra2@gmail.com>
2026-02-10 12:57:17 +0530

    setup server

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverConfig')
-
 
 const setupAndStartServer = async () => {
     // Create an instance of the Express application
     const app = express();
     // Middleware to parse JSON bodies
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
     
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
